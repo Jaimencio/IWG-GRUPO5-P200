296d834a8ec182b970d8c21efbfd61be24199fc6
Jaimencio <Jariasj@usm.cl>
2023-11-13 20:16:22 -0300

    añadido color verde

--- The Debuggers/pagina_web/pagina_web/settings.py
+++ The Debuggers/pagina_web/pagina_web/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'pagina_web/static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
